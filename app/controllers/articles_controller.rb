6422816324ad192a632823ea56b80cd4ec989406
DewangS <dewang.shahu@gmail.com>
2016-07-07 18:57:56 +1000

    Add, Edit, Update, show and index functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,59 @@
 class ArticlesController < ApplicationController
+
+def index
+  @articles = Article.all
+end
 def new
   @article = Article.new
 end
 def create
   #render plain: params[:article]
   @article = Article.new(article_params)
-  @article.save
-  redirect_to articles_show(@article)
+  if @article.save
+    flash[:success] = "Article was successfuly created."
+    redirect_to article_path(:id => @article.id)
+  else
+    @article.errors.full_messages.each do |msg|
+      flash[:error] = @article.errors.full_messages
+    end 
+    @article.errors.clear
+    render 'new'
+  end
 end
-def articles_show(article)
+def show
+  @article = Article.find(params[:id])
   render :show
 end
+
+def edit
+    @article = Article.find(params[:id])
+    #render :edit
+end
+
+def update
+  @article = Article.find(params[:id])
+  if @article.update(article_params)
+    flash[:success] = "Article was successfuly updated."
+    redirect_to article_path(:id => @article.id)
+  else
+    @article.errors.full_messages.each do |msg|
+      flash[:error] = @article.errors.full_messages
+    end 
+    @article.errors.clear
+    render 'edit'
+  end
+end
+
+def delete
+    article = Article.find(params[:id]).destroy
+    flash[:notice] = "Article '#{article.title}' successfully deleted."
+    redirect_to articles_path
+end
+
+def destroy
+  @article = Article.find(params[:id])
+end
+
 private
 def article_params
   params.require(:article).permit(:title, :description)
