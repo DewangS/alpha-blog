8266af7ca54fdfe793355b4c652028b342545a0a
DewangS <dewang.shahu@gmail.com>
2016-07-29 13:11:29 +1000

    Added User edit functionality using partials

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,24 @@ def create
   end
 end
 
+def edit
+  @user = User.find(params[:id])
+end
+
+def update
+  @user = User.find(params[:id])
+  if @user.update(user_params)
+    flash[:success] = "Your account was successfuly updated."
+    redirect_to articles_path
+  else
+    #@article.errors.full_messages.each do |msg|
+    #  flash[:danger] = @article.errors.full_messages
+    #end 
+    #@article.errors.clear
+    render 'edit'
+  end
+end
+
 private
 def user_params
     params.require(:user).permit(:username, :email, :password)
