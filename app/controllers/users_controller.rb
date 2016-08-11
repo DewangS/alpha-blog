0eb47aac1c2f7fc559a21bf639dbf8cd895b8dc4
DewangS <dewang.shahu@gmail.com>
2016-08-11 16:16:42 +1000

    Added singin upon new signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ end
 def create
   @user = User.new(user_params)
   if @user.save
+    session[:user_id] = @user.id
     flash[:success] = "Welcome #{@user.username} to the Alpha-Blog. Thanks for signing up."
-    redirect_to articles_path
+    redirect_to user_path(@user)
   else
     render 'new'
   end
